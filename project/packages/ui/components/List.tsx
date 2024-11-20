0c168fd242bcd1f04d7cfcf3ecdf7f1d05d1495f
VALAPALA MANOJ CHOWDHARY <manojkumar.valapala@concentrix.com>
2024-11-20 11:03:08 +0530

    Task-2: Parse the Pokémon list and display names using the List component

--- project/packages/ui/components/List.tsx
+++ project/packages/ui/components/List.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const List = ({ items }: { items: { name: string }[] }) => (
+interface Pokemon{
+    name : string;
+}
+
+interface ListProps{
+    items : Pokemon[]
+}
+
+
+const List: React.FC<ListProps> = ({ items }) => (
     <div className="p-4">
-        
         <ul className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
             {items.map((item) => (
                 <li
@@ -10,7 +18,7 @@ const List = ({ items }: { items: { name: string }[] }) => (
                     className="bg-white shadow-md hover:shadow-lg hover:bg-orangered hover:text-white rounded-lg p-4 text-center transition-transform transform hover:scale-105"
                 >
                     <p className="text-lg font-semibold capitalize">{item.name}</p>
-                </li>
+                    </li>
             ))}
         </ul>
     </div>
